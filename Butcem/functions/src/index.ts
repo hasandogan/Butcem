ee1fbb48f6bd9adab593d7cd3c923e19dc13a868
hasan-dogan <hasan.dogan@ideasoft.com.tr>
2025-01-07 19:29:32 +0300

    0.9

--- Butcem/functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from "firebase-admin";
-import {checkAndCreateNotifications} from "./functions";
-import {sendNotifications} from "./functions";
-
-admin.initializeApp();
-
-export {checkAndCreateNotifications};
-export {sendNotifications};
